0bbe957485888c8b3e7807eca81d816848e999c5
LALA TECH <olaniyanshamsudeenyinka001@gmail.com>
2025-10-28 13:56:27 +0100

    form api fixing

--- components/AdRequestForm.jsx
+++ components/AdRequestForm.jsx
@@ -23,7 +23,7 @@ export default function AdRequestForm({ isOpen, onClose }) {
     setLoading(true);
 
     try {
-      const response = await fetch("https://easyadbackend.onrender.com/api/ad-requests", {
+      const response = await fetch("https://easyadbackend.onrender.com/api/ad-request", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
