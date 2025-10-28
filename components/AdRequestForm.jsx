1f6ac09463fe7cfce7cb4fc30938fe5231bfbb88
LALA TECH <olaniyanshamsudeenyinka001@gmail.com>
2025-10-28 13:40:30 +0100

    new route

--- components/AdRequestForm.jsx
+++ components/AdRequestForm.jsx
@@ -23,7 +23,7 @@ export default function AdRequestForm({ isOpen, onClose }) {
     setLoading(true);
 
     try {
-      const response = await fetch("https://easyadbackend.onrender.com/api/adrequests", {
+      const response = await fetch("https://easyadbackend.onrender.com/api/ad-requests", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
